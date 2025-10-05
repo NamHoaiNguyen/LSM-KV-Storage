0715239d566432849360798bc864944bc7ee20cc
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2025-10-05 23:11:51 +0700

    Using AppendOnlyFile for manifest

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -79,7 +79,7 @@ bool DBImpl::LoadDB(std::string_view dbname) {
   // Load MANIFEST
   std::string manifest_path = db_path_ + kManifestFileName;
   manifest_write_object_ =
-      std::make_unique<io::LinuxWriteOnlyFile>(manifest_path);
+      std::make_unique<io::LinuxAppendOnlyFile>(manifest_path);
   if (!manifest_write_object_->Open()) {
     return false;
   }
@@ -455,7 +455,7 @@ void DBImpl::AddChangesToManifest(const VersionEdit *version_edit) {
 
   // TODO(namnh, IMPORTANT) : What if append fail ?
   // TODO(namnh, IMPORTANT) : Do we need to lock file ?
-  manifest_write_object_->AppendAtLast(bytes);
+  manifest_write_object_->Append(bytes);
 }
 
 void DBImpl::MaybeScheduleCompaction() {
